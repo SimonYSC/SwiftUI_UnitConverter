c0cbaab328369e38837574af8b389a30cd410818
Simon Chao <ysc406@nyu.edu>
2021-07-01 11:59:39 -0400

    Initial Commit

---
+++ UnitConverter/UnitConverterApp.swift
@@ -0,0 +1,17 @@
+//
+//  UnitConverterApp.swift
+//  UnitConverter
+//
+//  Created by YunShou Chao on 7/1/21.
+//
+
+import SwiftUI
+
+@main
+struct UnitConverterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
